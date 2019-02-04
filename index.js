c57c096b063da61ea7c6b3461988e8ab73d77bd9
Henry Koehler <henryk1229@gmail.com>
2019-02-04 22:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object,{ [key]: value })} 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object[key] = value; return object } 
 function deleteFromObjectByKey(object, key) { const newObj = Object.assign({}, Object); delete newObj.key; return newObj } 
-function destructivelyDeleteFromObjectByKey(object, key) { delete Object.key; return Object }
+function destructivelyDeleteFromObjectByKey(object, key) { delete Object['key']; return Object }
